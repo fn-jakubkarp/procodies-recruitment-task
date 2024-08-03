2a2a49d82e85d7e0541035f21b90752d854b5552
jakubkarp <quni.inne@gmail.com>
2024-08-04 01:56:03 +0200

    🖌️ chore: added texts for home page

---
+++ src/constants/texts.ts
@@ -0,0 +1,39 @@
+export const texts = {
+    header: {
+        intro: 'Chance to win a StockX Mystery Box today!',
+        subIntro: 'Unleash the Excitement of Mystery Electronics',
+    },
+    featureSection: {
+        tagline: 'Top Tech Mystery Box at Unbeatable Prices',
+        features:
+            'Fast shipping | Easy returns | Special discounts for students',
+    },
+    aboutSection: {
+        header: 'Curious about the StockX Mystery Box?',
+        firstParagraph:
+            'Experience the thrill of unboxing cutting-edge gadgets with Stockx Mystery Boxes. Each box is a treasure trove of high-quality electronics, handpicked to elevate your tech game.',
+        secondParagraph:
+            'Expect the unexpected! Our boxes are packed with the latest gadgets, from smartphones to gaming consoles, ensuring you get the best tech surprises every time.',
+    },
+    productSection: {
+        header: 'Why is everyone buzzing about these boxes?',
+        firstParagraph:
+            'StockX Mystery Boxes are not just about products; they’re about the experience. Imagine the excitement of unveiling top-tier electronics at a fraction of the cost.',
+        secondParagraph:
+            'Our boxes include a variety of premium tech items, making it a must-have for every tech enthusiast. Join the buzz and see why everyone is raving about the unbeatable value and surprise factor of Stockx Mystery Boxes!',
+    },
+    faqSection: {
+        firstQuestion: 'How can I win?',
+        secondQuestion: 'No hidden fees?',
+        thirdQuestion: 'Why do we need your billing information?',
+        firstAnswer:
+            'Thanks to your membership, you also participate in our contest to win the iPhone15. We sellect a winner among every 600 participants. The next winner will be selected on 06.10.2023, and will be notified directly by e-mail. If the reward is out of stock, the winner will receive a similar product of equal or greater value instead.',
+        secondAnswer:
+            'We ensure that our members are provided with a detailed history of transactions so that they know exactly what they are paying for. Credit card information is only required to facilitate future purchases. There will be no charges on your credit card statement if you do not upgrade to PREMIUM membership and do not make a purchase.',
+        thirdAnswer:
+            "By joining this service, you will receive a 5-day trial of our partner's program. If you continue with a subscription beyond the 5-day trial period, you will be charged an amount on your credit card which varies depending on the choice of the tariff. When your contribution is deducted from your credit card or other payment method, you will have access to these services which are reserved exclusively for members of our partner's website.",
+    },
+    footer: {
+        cards: 'We accept the following credit cards',
+    },
+};

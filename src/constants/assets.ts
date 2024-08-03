b1459b15d42a21e43b56694573a290f5746a0dec
jakubkarp <quni.inne@gmail.com>
2024-08-04 01:55:28 +0200

    ✨ feat: added assets from Figma

---
+++ src/constants/assets.ts
@@ -0,0 +1,21 @@
+import bit256_badge from '@/assets/bit256_badge.png';
+import footer_cards from '@/assets/footer_cards.png';
+import mcafree_badge from '@/assets/mcafree_badge.png';
+import truste_badge from '@/assets/truste_badge.png';
+import products_1 from '@/assets/products_1.png';
+import products_2 from '@/assets/products_2.png';
+import products_tb_1 from '@/assets/products_tb_1.png';
+import products_tb_2 from '@/assets/products_tb_2.png';
+import products_tb_3 from '@/assets/products_tb_3.png';
+
+export {
+    bit256_badge,
+    footer_cards,
+    mcafree_badge,
+    truste_badge,
+    products_1,
+    products_2,
+    products_tb_1,
+    products_tb_2,
+    products_tb_3,
+};

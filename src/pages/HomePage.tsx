cb5b19f700e2df6a818097153014e62f1a3d2068
jakubkarp <quni.inne@gmail.com>
2024-08-04 02:15:20 +0200

    ✨ feat: created Footer and Home page components

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import Footer from '@/components/ui/Footer';
+import React from 'react';
+
+interface HomePageProps {}
+
+const HomePage: React.FC<HomePageProps> = () => {
+    return (
+        <>
+            <Footer />
+        </>
+    );
+};
+
+export default HomePage;

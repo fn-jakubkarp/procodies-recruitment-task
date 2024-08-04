122be107a31a414b0979e40753222ef9ec5b2565
jakubkarp <quni.inne@gmail.com>
2024-08-04 21:18:59 +0200

    🎨 style: added bottom margins to imported components

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,35 +1,40 @@
+import Hero from '@/components/Hero';
+import Features from '@/components/Features';
 import Section from '@/components/Section';
 import Faq from '@/components/Faq';
 import Footer from '@/components/Footer';
-import React from 'react';
+
 import { texts } from '@/constants/texts';
 import { assets } from '@/constants/assets';
-import Features from '@/components/Features';
 
 const HomePage: React.FC = () => {
     return (
         <>
+            <Hero className="mb-12" />
             <Features
                 header={texts.featureSection.header}
                 features={texts.featureSection.features}
+                className="mb-8"
             />
             <Section
-                src={assets.products_1}
+                src={assets.products_2}
                 alt="AboutSection"
                 header={texts.aboutSection.header}
                 firstParagraph={texts.aboutSection.firstParagraph}
                 secondParagraph={texts.aboutSection.secondParagraph}
                 reverseLayout={true}
+                className="mb-12"
             />
             <Section
-                src={assets.products_2}
+                src={assets.products_1}
                 alt="productSection"
                 header={texts.productSection.header}
                 firstParagraph={texts.productSection.firstParagraph}
                 secondParagraph={texts.productSection.secondParagraph}
+                className="mb-12"
             />
-            <Faq />
-            <Footer />
+            <Faq className="mb-4" />
+            <Footer className="mb-4" />
         </>
     );
 };

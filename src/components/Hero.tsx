86e5f406588662653349c49605cc1227c210a3fe
jakubkarp <quni.inne@gmail.com>
2024-08-07 17:42:39 +0200

    ✨ feat: hero section w/ Gallery and Form component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,7 @@
-import FormComponent from './ui/FormComponent';
-
+import React, { useState } from 'react';
+import FormComponent from './FormComponent';
 import { texts } from '@/constants/texts';
 import { assets } from '@/constants/assets';
-
 import { cn } from '@/utils/twMerge';
 
 interface HeroProps {
@@ -10,25 +9,78 @@ interface HeroProps {
 }
 
 const Hero: React.FC<HeroProps> = ({ className }) => {
+    const [mainImage, setMainImage] = useState(assets.products_0);
+    const [fadeIn, setFadeIn] = useState(true);
+
+    const handleImageChange = (newImage: string) => {
+        setFadeIn(false);
+        setTimeout(() => {
+            setMainImage(newImage);
+            setFadeIn(true);
+        }, 200);
+    };
+
     return (
-        <>
-            <div className={cn('flex gap-4', className)}>
-                <div className="flex w-1/3">
+        <div id="wrapper" className={cn('flex gap-4 items-center', className)}>
+            <div
+                id="images"
+                className="flex flex-col flex-shrink-0 w-auto gap-4 min-w-[481px]"
+            >
+                <div className="relative">
+                    <div className="min-w-[481px] min-h-[480px]">
+                        <img
+                            src={mainImage}
+                            className={cn(
+                                'object-cover w-full h-full transition-opacity duration-200 ease-in-out',
+                                fadeIn ? 'opacity-100' : 'opacity-50',
+                            )}
+                            alt="Multiple tech gadgets spread over image"
+                            draggable="false"
+                        />
+                    </div>
+                    <div className="absolute right-0 top-0">
+                        <div className="w-[160px] h-[160px] rounded-full bg-accent flex flex-col justify-center items-center">
+                            <span className="text-white text-[2.375rem] font-bold">
+                                $1.99
+                            </span>
+                            <span className="text-white text-sm">
+                                Original value $500
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-between">
                     <img
-                        src={assets.products_1}
-                        className="w-full h-full object-cover"
-                        alt="Hero picture"
+                        src={assets.products_tb_1}
+                        className="object-cover ring-inset ring-[0.93px] rounded-[13.94px] border-accent cursor-pointer"
+                        alt="Multiple tech gadgets spread over image"
+                        draggable="false"
+                        onClick={() => handleImageChange(assets.products_tb_1)}
+                    />
+                    <img
+                        src={assets.products_tb_2}
+                        className="object-cover ring-inset ring-[0.93px] rounded-[13.94px] border-accent cursor-pointer"
+                        alt="Multiple tech gadgets spread over image"
+                        draggable="false"
+                        onClick={() => handleImageChange(assets.products_tb_2)}
+                    />
+                    <img
+                        src={assets.products_tb_3}
+                        className="object-cover ring-inset ring-[0.93px] rounded-[13.94px] border-accent cursor-pointer"
+                        alt="Multiple tech gadgets spread over image"
+                        draggable="false"
+                        onClick={() => handleImageChange(assets.products_tb_3)}
                     />
                 </div>
-                <div className="flex w-2/3 flex-col gap-4">
-                    <h1 className="font-extrabold text-[3.25rem] max-w-[90%]">
-                        {texts.hero.header}
-                    </h1>
-                    <h2 className="text-[1.375rem]">{texts.hero.subheader}</h2>
-                    <FormComponent />
-                </div>
             </div>
-        </>
+            <div id="form" className="flex flex-col w-full">
+                <h1 className="font-extrabold text-[3.25rem] mb-2">
+                    {texts.hero.header}
+                </h1>
+                <h2 className="text-[1.375rem] mb-6">{texts.hero.subheader}</h2>
+                <FormComponent />
+            </div>
+        </div>
     );
 };
 

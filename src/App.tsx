36b0712a3986712a7c17ed4c792dca769922f4b5
jakubkarp <quni.inne@gmail.com>
2024-08-07 17:43:08 +0200

    🖌️ style: applied max-w to the App

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import HomePage from './pages/HomePage';
 
 function App() {
     return (
-        <>
+        <main className="max-w-[1232px] flex flex-col items-center mx-auto">
             <HomePage />
-        </>
+        </main>
     );
 }
 
